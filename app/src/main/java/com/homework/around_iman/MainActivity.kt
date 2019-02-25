70127f7673e4b36d9e7a29e8321b9ce6880e3a93
iman <iman.fa88@yahoo.com>
2019-02-25 16:54:40 +0330

    first

---
+++ app/src/main/java/com/homework/around_iman/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.around_iman
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
