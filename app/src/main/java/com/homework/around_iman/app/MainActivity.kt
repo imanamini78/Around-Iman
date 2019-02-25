6460aedc6ad4805e65ed0c228a8828b136bd8373
iman <iman.fa88@yahoo.com>
2019-02-25 18:16:05 +0330

    start MainActivity

--- app/src/main/java/com/homework/around_iman/app/MainActivity.kt
+++ app/src/main/java/com/homework/around_iman/app/MainActivity.kt
@@ -2,7 +2,13 @@ package com.homework.around_iman.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.homework.around_iman.R
+import com.homework.around_iman.data.PlaceRepository
+import com.homework.around_iman.data.model.PlaceEntity
+import com.homework.around_iman.remote.NetworkManager
+import com.homework.around_iman.remote.datasource.PlaceDataSource
+import com.homework.around_iman.remote.service.PlaceService
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        val placeRepository = PlaceRepository(PlaceDataSource(PlaceService(NetworkManager())))
+        placeRepository.getFeaturedPlaces(::onFeaturedPlace)
+    }
+    private fun onFeaturedPlace(list: List<PlaceEntity>?) = runOnUiThread {
+        Toast.makeText(this,list.toString(),Toast.LENGTH_LONG).show()
     }
 }
+
